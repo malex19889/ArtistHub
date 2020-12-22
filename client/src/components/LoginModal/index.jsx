15a14f61a3c10f36741bb287400b96f2b842f1d0
Celine Ross <celineross@gmail.com>
2020-12-21 19:54:45 -0600

    moved modal bodies into components

--- client/src/components/LoginModal/index.jsx
+++ client/src/components/LoginModal/index.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 export default function Modal(props) {
     const style = {
@@ -24,7 +29,29 @@ export default function Modal(props) {
     return <div style={style.modal}>
         <div style={style.content}>
             <button onClick={props.handleLoginModal}>X</button>
-            {props.children}
+            <Container>
+            <Row className="justify-content-lg-center">
+              <Col style={{ maxWidth: "500px", marginTop: "30px", marginBottom: "30px" }}>
+                <Form className="bandregister">
+
+                  <h1>Login</h1>
+
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="username" placeholder="Enter username" />
+                  </Form.Group>
+
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" href="/user/settings">
+                    Submit
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
         </div>
     </div>
 }
