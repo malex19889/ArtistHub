73737790609468a9cb354e43fb21c3c8969ab777
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-12-23 19:15:45 -0600

    made changes to bandImages to create the routes

--- models/bandImage.js
+++ models/bandImage.js
@@ -1,21 +1,19 @@
 module.exports = function (sequelize, DataTypes) {
-  var BandImages = sequelize.define("BandImages", {
+  var BandImage = sequelize.define("BandImage", {
     images: {
       type: DataTypes.BLOB,
       allowNull: false,
       unique: false,
-      validate: {
-        len: [1]
-      }
     },
   });
-  BandImages.associate = (models) => {
-    BandImages.belongsTo(models.BandUser, {
+  BandImage.associate = (models) => {
+    BandImage.belongsTo(models.BandUser, {
       foreignKey: {
         allowNull: false
       }
     });
   };
 
-  return BandImages;
+
+  return BandImage;
 };
