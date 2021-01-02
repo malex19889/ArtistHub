7ae5659d699b961b981a909cbc237195b44c4643
Pete Hernandez <pete.g.hernandez@gmail.com>
2021-01-02 12:16:50 -0600

    updated

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,5 @@ DROP DATABASE IF EXISTS ArtistHub;
 CREATE DATABASE ArtistHub;
 
 
-INSERT INTO Users(id, firstName, lastName, userName, email, password, createAt, updateAt)
-VALUES (1, "D.B.", "Cooper", "dbcooper71", "dbcooper@gmail.com", "11041971", NOW(), NOW()),
-(2, "Tony", "Montana", "theworldisyours", "tony.montana@gmail.com", "87879", NOW(), NOW()),
-(3, "Pablo", "Escobar", "money", "paescobar@yahoo.com", "287987987", NOW(), NOW()),
-(4, "Joaquin", "Guzman", "ElChapo", "jguzman@hotmail.com", "7879621", NOW(), NOW());
+
 
