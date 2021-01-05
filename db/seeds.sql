fefa48d35191202f555b8afae9497dd86b808765
Pete Hernandez <pete.g.hernandez@gmail.com>
2021-01-04 21:52:26 -0600

    updated the capital letters

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,7 +17,7 @@ USE artisthub;
         -- we will need to be able to pull all data from the band ID so we can display the data in Favorite Card.
 
 -- User table 
-INSERT INTO users(firstName, lastName, userName, email, password, createdAt, updateAt)
+INSERT INTO users(firstName, lastName, userName, email, password, createdAt, updatedAt)
 VALUES ("D.B.", "Cooper", "dbcooper71", "dbcooper@gmail.com", "11041971", NOW(), NOW()),
 ("Tony", "Montana", "theworldisyours", "tony.montana@gmail.com", "87879", NOW(), NOW()),
 ("Pablo", "Escobar", "money", "paescobar@yahoo.com", "287987987", NOW(), NOW()),
@@ -26,14 +26,14 @@ VALUES ("D.B.", "Cooper", "dbcooper71", "dbcooper@gmail.com", "11041971", NOW(),
 
 
 -- TourDate Table
-INSERT INTO tourDates(date, time, location, ticketPrice, note,  createAt, updateAt)
+INSERT INTO tourDates(date, time, location, ticketPrice, note,  createdAt, updatedAt)
 VALUES ("2021-03-22", "04:30", "San Antonio Tx", "$20-$100", "BYOB", NOW(), NOW()),
 ("2021-05-15", "07:00", "Austin Tx", "$30-$75", "bring chairs and BYOB", NOW(), NOW()),
 ("2021-04-10", "08:30", "Austin Tx", "$20-$75", "bring chairs and BYOB", NOW(), NOW()),
 ("2021-05-18", "07:40", "San Antonio Tx", "$10-$65", "BYOB", NOW(), NOW());
 
 -- bandUser Table
-INSERT INTO bandUsers(bandName, bandBio, userName, email, genre, contact, youtube, facebook, insta, twitter, password, createdAt, updateAt)
+INSERT INTO bandUsers(bandName, bandBio, userName, email, genre, contact, youtube, facebook, insta, twitter, password, createdAt, updatedAt)
 VALUES ("Coding Badly", "All about the code", "Funcoding", "codingbad@gmail.com", "rock", "youtube.com", "instagram.com", "twitter.com", "676673", NOW(), NOW()),
 ("Bad Music", "tickling the ivory", "pianostrong", "babygrand@gmail.com", "classic", "youtube.com", "instagram.com", "twitter.com", "847473",  NOW(), NOW()),
 ("Archtop Players", "Struming around", "steelguitars","playingthesteel@gmail.com", "rock", "youtube.com", "instagram.com", "twitter.com", "473",  NOW(), NOW()),
@@ -42,7 +42,7 @@ VALUES ("Coding Badly", "All about the code", "Funcoding", "codingbad@gmail.com"
 
 
 -- bandMember Table
-INSERT INTO bandMembers(firstName, lastName, contact, bandRole, facebook, insta, twitter, createAt, updateAt)
+INSERT INTO bandMembers(firstName, lastName, contact, bandRole, facebook, insta, twitter, createdAt, updatedAt)
 VALUES ("Johnny", "Rocker", "938-938-8766", "drummer", "facebook.com", "instagram.com", "twitter.com", NOW(), NOW()),
 ("Flash", "Dancer", "392-538-8966", "guitar", "facebook.com", "instagram.com", "twitter.com", NOW(), NOW()),
 ("Ivory", "Player", "938-548-9336", "piano", "facebook.com", "instagram.com", "twitter.com", NOW(), NOW()),
