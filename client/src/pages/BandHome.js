da0f88cee806baa5e95c1a9204a574565d610937
Pete Hernandez <pete.g.hernandez@gmail.com>
2021-01-06 23:41:10 -0600

    get the latest pull

--- client/src/pages/BandHome.js
+++ client/src/pages/BandHome.js
@@ -18,67 +18,67 @@ import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 // useParams to set id
 export default function BandHome() {
-    const band = {
-        firstName: "Ozzy",
-        lastName: "Osbourne",
-        bandName: "Black Sabbath",
-        bandBio: "Black Sabbath were an English rock band formed in Birmingham in 1968 by guitarist Tony Iommi, drummer Bill Ward, bassist Geezer Butler and vocalist Ozzy Osbourne. They are often cited as pioneers of heavy metal music. The band helped define the genre with releases such as Black Sabbath (1970), Paranoid (1970), and Master of Reality (1971). The band had multiple line-up changes following Osbourne's departure in 1979, with Iommi being the only constant member throughout its history.",
-        genre: "Doom",
-        bannerImage: "",
-        contact: "2134567",
-        youtube: "https://www.youtube.com/",
-        facebook: "https://www.facebook.com/",
-        insta: "https://www.instagram.com/",
-        twitter: "https://twitter.com/",
-        bandMembers: [
-            {
-                firstName: "Tony",
-                lastName: "Iommi",
-                contact: "6368425",
-                bandRole: "Guitar",
-                facebook: "https://www.facebook.com/",
-                insta: "https://www.instagram.com/",
-                twitter: "https://twitter.com/"
-            },
-            {
-                firstName: "Geezer",
-                lastName: "Butler",
-                contact: "3792674",
-                bandRole: "Bass",
-                facebook: "https://www.facebook.com/",
-                insta: "https://www.instagram.com/",
-                twitter: "https://twitter.com/"
-            },
-            {
-                firstName: "Bill",
-                lastName: "Ward",
-                contact: "2693573",
-                bandRole: "Drums",
-                facebook: "https://www.facebook.com/",
-                insta: "https://www.instagram.com/",
-                twitter: "https://twitter.com/"
-            }
-        ],
+    // const band = {
+    //     firstName: "Ozzy",
+    //     lastName: "Osbourne",
+    //     bandName: "Black Sabbath",
+    //     bandBio: "Black Sabbath were an English rock band formed in Birmingham in 1968 by guitarist Tony Iommi, drummer Bill Ward, bassist Geezer Butler and vocalist Ozzy Osbourne. They are often cited as pioneers of heavy metal music. The band helped define the genre with releases such as Black Sabbath (1970), Paranoid (1970), and Master of Reality (1971). The band had multiple line-up changes following Osbourne's departure in 1979, with Iommi being the only constant member throughout its history.",
+    //     genre: "Doom",
+    //     bannerImage: "",
+    //     contact: "2134567",
+    //     youtube: "https://www.youtube.com/",
+    //     facebook: "https://www.facebook.com/",
+    //     insta: "https://www.instagram.com/",
+    //     twitter: "https://twitter.com/",
+    //     bandMembers: [
+    //         {
+    //             firstName: "Tony",
+    //             lastName: "Iommi",
+    //             contact: "6368425",
+    //             bandRole: "Guitar",
+    //             facebook: "https://www.facebook.com/",
+    //             insta: "https://www.instagram.com/",
+    //             twitter: "https://twitter.com/"
+    //         },
+    //         {
+    //             firstName: "Geezer",
+    //             lastName: "Butler",
+    //             contact: "3792674",
+    //             bandRole: "Bass",
+    //             facebook: "https://www.facebook.com/",
+    //             insta: "https://www.instagram.com/",
+    //             twitter: "https://twitter.com/"
+    //         },
+    //         {
+    //             firstName: "Bill",
+    //             lastName: "Ward",
+    //             contact: "2693573",
+    //             bandRole: "Drums",
+    //             facebook: "https://www.facebook.com/",
+    //             insta: "https://www.instagram.com/",
+    //             twitter: "https://twitter.com/"
+    //         }
+    //     ],
 
-        tour: [
-            {
-                tourName: "Summer Slaughter Tour",
-                date: "July 10, 2021",
-                time: "9 PM",
-                location: "Stubb's BBQ, Austin, TX",
-                ticketPrice: "$15",
-                notes: "BYOB, 1 hour set, opener has cancelled and we'll be going on at 10 PM."
-            },
-            {
-                tourName: "Summer Slaughter Tour",
-                date: "July 12, 2021",
-                time: "9 PM",
-                location: "Zombies, San Antonio, TX",
-                ticketPrice: "$20",
-                notes: "Open bar. Please bring ID, 21+ show."
-            }
-        ]
-    };
+    //     tour: [
+    //         {
+    //             tourName: "Summer Slaughter Tour",
+    //             date: "July 10, 2021",
+    //             time: "9 PM",
+    //             location: "Stubb's BBQ, Austin, TX",
+    //             ticketPrice: "$15",
+    //             notes: "BYOB, 1 hour set, opener has cancelled and we'll be going on at 10 PM."
+    //         },
+    //         {
+    //             tourName: "Summer Slaughter Tour",
+    //             date: "July 12, 2021",
+    //             time: "9 PM",
+    //             location: "Zombies, San Antonio, TX",
+    //             ticketPrice: "$20",
+    //             notes: "Open bar. Please bring ID, 21+ show."
+    //         }
+    //     ]
+    // };
     const [authState, dispatch] = useAuthContext();
 
     
@@ -89,14 +89,14 @@ export default function BandHome() {
        API.bandInfoById(id)
        .then((res) => {
            console.log(res)
-        //    setBandState(res)
+           setBandState(res)
        })
        .catch(err=> console.log(err))
     })
 
-    function handleFavorite(band) {
+    function handleFavorite(res) {
         console.log(authState)
-        const obj = {band, authState}
+        const obj = {res, authState}
         API.saveFavorites(obj)
       }
 
@@ -114,18 +114,18 @@ export default function BandHome() {
                 </Nav.Item>
             </Navibar>
 
-            <BandJumbotron band={band} />
+            <BandJumbotron band={bandState} />
             <Container fluid>
                 <Row>
                     <Col lg={3} style={{marginTop:"10px", marginBottom:"10px"}}>
-                        <BandBioCard band={band} />
-                        <FavBtn handleFavorite={handleFavorite} band={band} />
+                        <BandBioCard band={bandState} />
+                        <FavBtn handleFavorite={handleFavorite} band={bandState} />
                     </Col>
                     <Col lg={5} style={{marginTop:"10px", marginBottom:"10px"}}>
-                        <EventsGroup band={band} />
+                        <EventsGroup band={bandState} />
                     </Col>
                     <Col lg={4} style={{marginTop:"10px", marginBottom:"10px"}}>
-                        <ContactCard band={band} />
+                        <ContactCard band={bandState} />
                     </Col>
                 </Row>
             </Container>
