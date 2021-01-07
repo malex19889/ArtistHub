93259002476a84ed4e20917b89d9db31e76c8735
malex19889 <malex19889@yahoo.com>
2021-01-07 17:28:17 -0600

    wireing up favorite delete button

--- client/src/components/DeleteBtn/index.jsx
+++ client/src/components/DeleteBtn/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./style.css";
-
+import API from "../../utils/API"
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
+
+  function handleDeleteFavorite(id) {
+    console.log("hey",props)
+        API.deleteFavorites(id)
+}
   return (
     <button className="delete-btn btn btn-dark" 
-    onClick={() => props.handleDeleteFavorite(props.id)}>
+    onClick={()=>handleDeleteFavorite(props.id)}>
       Delete
     </button>
   );
