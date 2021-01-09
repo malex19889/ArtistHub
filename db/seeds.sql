a229fce757bb3175c3ab65368ee18def72cc78d1
ryan hatfield <hatfield086@gmail.com>
2021-01-09 08:44:08 -0600

    merge

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,6 @@
 USE artisthub;
 
-
--- notes about how to improve our DB structure, as there is a lot of redundancy and overcomplication:
-
--- id needs to be deleted for all models, and removed from seeds.
--- sequelize generates the id for us
-
--- bandImage should be moved into the bandUser page, doesn't need to be separate
--- Foreign Keys should not be defined here at all (see "FOREIGNKEY", take it out). 
--- Foreign Keys should instead have the BandUser ID added onto their models. i.e., bandImage, tourDate, and Favorites should just have the bandUser ID attached to them for association.
--- model should be changed, if time allows, to band, user, tourDate, and favorite.
-    -- band will include all of the information already in bandUser, but add bandImage
-    -- user will remain the same.
-    -- tourDate will contain the same model, but also include the band ID.
-    -- favorite will be almost the same, but only needs to take in the band ID and user's ID.
-        -- we will need to be able to pull all data from the band ID so we can display the data in Favorite Card.
-
 -- User table 
--- TourDate Table
-
 INSERT INTO users(firstName, lastName, userName, email, password, createdAt, updatedAt)
 VALUES ("D.B.", "Cooper", "dbcooper71", "dbcooper@gmail.com", "11041971", NOW(), NOW()),
 ("Tony", "Montana", "theworldisyours", "tony.montana@gmail.com", "87879", NOW(), NOW()),
