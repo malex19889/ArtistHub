f40474b5957525afa85b25f72e26686fc0033f3c
Celine Ross <celineross@gmail.com>
2021-01-22 21:46:24 -0600

    fixed navigation, and added modal and modal hook

--- client/src/pages/BandHome.js
+++ client/src/pages/BandHome.js
@@ -9,15 +9,21 @@ import Navibar from "../components/Navibar";
 import Logout from "../components/LogoutBtn";
 import ContactCard from "../components/ContactCard";
 import FavBtn from "../components/FavBtn";
+import ModalA from "../components/Modal";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
-import API from "../utils/API"
+import useModal from "../hooks/useModal";
+
+import API from "../utils/API";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
-// useParams to set id
+
 export default function BandHome() {
+    const { isShown, toggle } = useModal();
 
     const [authState, dispatch] = useAuthContext();
     const [band, setBand] = useState({
@@ -98,14 +104,86 @@ export default function BandHome() {
         API.saveFavorites(obj);
     }
 
-    return (
+    if (!authState.loggedIn) {
+        return (
+            <div>
+                <Navibar>
+                    <ModalA
+                        onClick={toggle}
+                        isShown={isShown}
+                        hide={toggle}
+                        loginRegister="Login">
+                        <h2>Login</h2>
+                        <Login></Login>
+                    </ModalA>
+
+                    <ModalA
+                        onClick={toggle}
+                        isShown={isShown}
+                        hide={toggle}
+                        loginRegister="Register">
+                        <h2>Register</h2>
+                        <Register></Register>
+                    </ModalA>
+                </Navibar>
+
+                <BandJumbotron band={band} />
+                <Container fluid>
+                    <Row>
+                        <Col lg={3} style={{ marginTop: "10px", marginBottom: "10px" }}>
+                            <BandBioCard band={band} />
+                        </Col>
+                        <Col lg={5} style={{ marginTop: "10px", marginBottom: "10px" }}>
+                            <EventsGroup band={band} />
+                        </Col>
+                        <Col lg={4} style={{ marginTop: "10px", marginBottom: "10px" }}>
+                            <ContactCard band={band} />
+                        </Col>
+                    </Row>
+                </Container>
+
+            </div>
+        );
+    } else if (authState.loggedIn && authState.isBand) {
+        return (
+            <div>
+                <Navibar>
+                    <Nav.Item>
+                        <Nav.Link style={{ color: "white" }} href={"/band/home/" + band.id}>My Band</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link style={{ color: "white" }} href={"/band/settings/" + band.id}>Settings</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Logout>Logout</Logout>
+                    </Nav.Item>
+                </Navibar>
+
+                <BandJumbotron band={band} />
+                <Container fluid>
+                    <Row>
+                        <Col lg={3} style={{ marginTop: "10px", marginBottom: "10px" }}>
+                            <BandBioCard band={band} />
+                        </Col>
+                        <Col lg={5} style={{ marginTop: "10px", marginBottom: "10px" }}>
+                            <EventsGroup band={band} />
+                        </Col>
+                        <Col lg={4} style={{ marginTop: "10px", marginBottom: "10px" }}>
+                            <ContactCard band={band} />
+                        </Col>
+                    </Row>
+                </Container>
+
+            </div>
+        )
+    } else return (
         <div>
             <Navibar>
                 <Nav.Item>
-                    <Nav.Link style={{ color: "white" }} href={"/band/home/" + band.id}>My Band</Nav.Link>
+                    <Nav.Link style={{ color: "white" }} href={"/user/favorites/" + authState.id}>Favorites</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link style={{ color: "white" }} href={"/band/settings/" + band.id}>Settings</Nav.Link>
+                    <Nav.Link style={{ color: "white" }} href={"/user/settings" + authState.id}>Settings</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Logout>Logout</Logout>
@@ -117,7 +195,7 @@ export default function BandHome() {
                 <Row>
                     <Col lg={3} style={{ marginTop: "10px", marginBottom: "10px" }}>
                         <BandBioCard band={band} />
-                        {authState.isBand ? <div></div> : <FavBtn handleFavorite={handleFavorite} band={band} />}
+                        <FavBtn handleFavorite={handleFavorite} band={band} />
                     </Col>
                     <Col lg={5} style={{ marginTop: "10px", marginBottom: "10px" }}>
                         <EventsGroup band={band} />
@@ -129,6 +207,5 @@ export default function BandHome() {
             </Container>
 
         </div>
-    );
-
+    )
 }
