1fb56d1e461de430ffee9a8b3c84d8b918e978cd
Celine Ross <celineross@gmail.com>
2021-01-23 20:22:09 -0600

    working on merch page

--- client/src/components/MerchCard/index.jsx
+++ client/src/components/MerchCard/index.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
+import useHandleInputChange from "../../hooks/useHandleInputChange";
 
+export default function MerchCard(props) {
 
-export default function MerchCard() {
+    const {state, handleInputChange} = useHandleInputChange();
+    
     return (
-            <Card style={{ width: '18rem', margin:"10px" }}>
-                <Card.Img variant="top" src="https://via.placeholder.com/80" />
-                <Card.Body>
-                    <Card.Title>Merch Item</Card.Title>
-                    <Card.Text>
-                        Short description of merch item.
+        <Card style={{ width: '18rem', margin: "10px" }}>
+            <Card.Img variant="top" src="https://via.placeholder.com/80" />
+            <Card.Body>
+                <Card.Title>{props.merchItem}</Card.Title>
+                <Card.Text>
+                    {props.merchDesc}
                 </Card.Text>
-                    <Button variant="dark">Purchase</Button>
-                </Card.Body>
-            </Card>
-
+                <Button variant="dark">Purchase</Button>
+            </Card.Body>
+        </Card>
     );
 }
